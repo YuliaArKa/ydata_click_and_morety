b7154feb9348bd162f6d831d86c7dc5d73b9a376
YuliaArKa <yulianudelman@gmail.com>
2025-02-14 15:18:57 +0200

    update path

--- app/app.py
+++ app/app.py
@@ -173,7 +173,6 @@ def main():
     st.set_page_config(page_title="CTR Click Prediction Evaluation", layout="wide")
     st.title("CTR Click Prediction Evaluation App")
 
-    BASE_DIR = Path(__file__).resolve().parent
     # Define fixed paths for training (used in preprocessing) and evaluation test set.
     #TRAIN_PATH = "../data/train_dataset_full.csv"  # update if needed
     #EVAL_TEST_PATH = "../data/X_test_1st.csv"         # used for evaluation tab
@@ -181,10 +180,10 @@ def main():
     #MODEL_PATH = '../models/XGBoost.joblib'
 
     # Define fixed paths using the pathlib syntax
-    TRAIN_PATH = BASE_DIR / "data" / "train_dataset_full.csv"
-    EVAL_TEST_PATH = BASE_DIR / "data" / "X_test_1st.csv"
-    LABEL_TEST_PATH = BASE_DIR / "data" / "y_test_1st.csv"
-    MODEL_PATH = BASE_DIR / "models" / "XGBoost.joblib"
+    TRAIN_PATH = project_root / "data" / "train_dataset_full.csv"
+    EVAL_TEST_PATH = project_root / "data" / "X_test_1st.csv"
+    LABEL_TEST_PATH = project_root / "data" / "y_test_1st.csv"
+    MODEL_PATH = project_root / "models" / "XGBoost.joblib"
     # Create two tabs: one for uploading a test set & scoring, and one for model evaluation.
     tab_upload, tab_eval = st.tabs(["Upload & Score", "Model Evaluation"])
     
